069e451401ed0eca4dac4b6fc76ff396045f7267
hp <hp@Laptop-Jira>
2025-11-13 18:18:00 +0530

    Changes made to POM surefire added. Correct package name in the xml file

--- Githubtest/src/test/java/Jenkinsgit/another.java
+++ Githubtest/src/test/java/Jenkinsgit/another.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class another {
 public WebDriver driver;
